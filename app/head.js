657a0c556cd406b9599a0b8463466fe5755939e1
Pritish Kumar Kar <65906403+pr0digy1337@users.noreply.github.com>
2024-07-20 17:38:07 +0530

    Add files via upload

---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>Innovation Center</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/ic.ico" />
+  </>
+);
+
+export default Head;
